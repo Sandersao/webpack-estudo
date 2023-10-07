0b5ce896ae58d9b4ccdcd8ab4fff0558bebf9089
Sanderson A. Rocha <sanduba753@gmail.com>
2023-10-07 16:02:58 -0300

    Configuration of the css and html webpack loading modulres
    Configuration of the CSS webpack loading modules
    Configuration of the HTML webpack loading module
    Instalation of pertinent modules

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,7 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      { test: /\.html$/i, loader: "html-loader" },
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Webpack implementation'
+  })],
   mode: 'development'
   // mode: 'production'
 };
