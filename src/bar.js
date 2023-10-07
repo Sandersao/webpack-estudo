f84a622684b136810800502e5287139805d09c6f
Sanderson A. Rocha <sanduba753@gmail.com>
2023-10-07 15:10:16 -0300

    Removal of autorefresh rutine

--- src/bar.js
+++ src/bar.js
@@ -1,3 +1,3 @@
-export default function bar() {
+export default () => {
     console.log('Teste');
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-
-import autoAtualizar from './adapter/autoAtualizar.js';
 import bar from './bar.js';
-
-autoAtualizar();
 
 bar();
